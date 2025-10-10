db04e2bcf771396e27ee3b61e895b5a1c2918b25
bafc13 <art.bryzghalin@mail.ru>
2025-10-10 20:42:34 +0300

    created class normalizer, onCreate was completed

--- app/src/main/java/com/example/lab1_bafc13/MainActivity.kt
+++ app/src/main/java/com/example/lab1_bafc13/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lab1_bafc13
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val normalizer = Normalizer()
+        val initialList = normalizer.fillList()
+        val initialString = normalizer.makeString(initialList)
+        val textView = findViewById<EditText>(R.id.initial_list)
+        textView.setText(initialString)
+
+        val button = findViewById<Button>(R.id.normalize_button)
+        button.setOnClickListener {
+            val normalizedList = normalizer.normalizeByString(textView.text.toString())
+            val normalizedString = normalizer.makeString(normalizedList)
+            val textView = findViewById<TextView>(R.id.normalized_list)
+            textView.text = normalizedString
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/lab1_bafc13/Normalizer.kt
@@ -0,0 +1,27 @@
+package com.example.lab1_bafc13
+
+import kotlin.math.floor
+
+class Normalizer {
+    val listToNormalize = mutableListOf<Int>()
+    val normalizedList = mutableListOf<Double>()
+    fun fillList(): MutableList<Int> {
+        return MutableList(10) { (1..125).random() }
+    }
+    fun <T> makeString(data: MutableList<T>): String {
+        return data.joinToString(", ")
+    }
+    fun normalizeByString(data: String): MutableList<Double> {
+        listToNormalize.clear()
+        listToNormalize.addAll( data.split(",")
+            .map { it.trim() }
+            .map { it.toInt() } )
+        normalizeList(listToNormalize)
+        return normalizedList
+    }
+    fun normalizeList(data: List<Int>){
+        val maxVal = data.maxOrNull() ?: 1
+        normalizedList.clear()
+        normalizedList.addAll( data.map { floor(( it.toDouble() / maxVal) * 1000) / 1000 } as MutableList<Double> )
+    }
+}
