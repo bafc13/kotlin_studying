68f6c160b644a06e4fd412a46412d8108c1d343f
bafc13 <art.bryzghalin@mail.ru>
2025-12-11 16:20:29 +0300

    unnecessary debug toasts removed, added night theme, added icons, colors, attrs.xml file, that provides color references to the themes colors. themes colors, in turn, were specified in themes.xml.

--- app/src/main/java/com/example/lab1_bafc13/REST/RetrofitClient.kt
+++ app/src/main/java/com/example/lab1_bafc13/REST/RetrofitClient.kt
@@ -7,7 +7,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "http://192.168.0.98:3000/"
 
-
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
